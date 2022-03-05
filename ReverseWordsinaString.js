fe657ede645fa2755966b48fc93bf1a51a50413b
Preity24 <40320160+Preity24@users.noreply.github.com>
2022-03-04 23:33:26 -0500

    Update ReverseWordsinaString.js

--- ReverseWordsinaString.js
+++ ReverseWordsinaString.js
@@ -3,8 +3,13 @@
  * @return {string}
  */
 var reverseWords = function(s) {
+    // Split the string into array
     let word = s.split(" ");
+    // Loop through each word in an array, split, reverse and join
     let reverseWord = word.map(item => item.split("").reverse().join(""));
     return reverseWord.join(" ");
     
 };
+
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
