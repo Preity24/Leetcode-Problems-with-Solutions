073ff54374a1f6b7995c58631bb4f213b209b18b
Preity24 <40320160+Preity24@users.noreply.github.com>
2022-03-05 14:02:25 -0500

    Add files via upload

--- ReverseString.js
+++ ReverseString.js
@@ -2,6 +2,21 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function(s) {
-    s.reverse().join("");    
+ var reverseString = function(s) {
+    // With inbuilt function
+   // s.reverse().join("");  
+    
+    
+    //Without inbuilt function
+    
+    let first = 0;
+    let last = s.length - 1;
+    
+    while(first <= last){
+        let temp = s[first];
+        s[first] = s[last];
+        s[last] = temp;
+        first++;
+        last--;
+    }   
 };
